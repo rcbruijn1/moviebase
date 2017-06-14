588596bb1cd5879a0e96f99841f3385fcd895bdb
ddleblanc <lxblvnc@gmail.com>
2017-06-15 01:48:38 +0200

    Login toegevoegd
    De login functionaliteit werkt met een dummy username & wachtwoord. username = daniel, wachtwoord is leblanc.

---
+++ app/src/main/java/rbdb/moviebase/Service/Config.java
@@ -0,0 +1,13 @@
+package rbdb.moviebase.Service;
+
+/**
+ * Bevat o.a. URL definities.
+ */
+public class Config {
+
+    private static final String BASIC_URL = "https://ticketflowserver.herokuapp.com";
+
+    public static final String URL_LOGIN = BASIC_URL + "/api/v1/login";
+    public static final String URL_TODOS = BASIC_URL + "/api/v1/todos";
+
+}
--- app/src/main/java/rbdb/moviebase/presentation/MainActivity.java
+++ app/src/main/java/rbdb/moviebase/presentation/MainActivity.java
@@ -1,7 +1,9 @@
-package rbdb.moviebase;
+package rbdb.moviebase.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import rbdb.moviebase.R;
 
 public class MainActivity extends AppCompatActivity {
 
