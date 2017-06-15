549143fd6da851e48a65964b07887654094bcc09
Ruben <rcbruijn1@avans.nl>
2017-06-15 13:25:26 +0200

    routes toegevoegd

--- app/src/main/java/rbdb/moviebase/Service/Config.java
+++ app/src/main/java/rbdb/moviebase/Service/Config.java
@@ -8,6 +8,7 @@ public class Config {
     private static final String BASIC_URL = "https://ticketflowserver.herokuapp.com";
 
     public static final String URL_LOGIN = BASIC_URL + "/api/v1/login";
-    public static final String URL_TODOS = BASIC_URL + "/api/v1/todos";
+    public static final String URL_REGISTER = BASIC_URL + "/api/v1/register";
+    public static final String URL_MOVIES = BASIC_URL + "/api/v1/movies";
 
 }
